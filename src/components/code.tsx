39c3e837ded6c24de4781e15866b37059ea0e08f
david <davidLiao@hahow.in>
2023-04-20 19:11:47 +0800

    fix: add fallback to prevent hljs error

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,15 +1,24 @@
 import hljs from 'highlight.js/lib/common'
 import 'highlight.js/styles/github.css'
+import { useMemo } from 'react'
 
 const Code = ({ children, language = 'javascript' }) => {
+  const htmlText = useMemo(() => {
+    try {
+      return hljs.highlight(children, {
+        language: language.toLowerCase() || 'javascript',
+      }).value
+    } catch {
+      return children
+    }
+  }, [children, language])
+
   return (
     <>
       <pre>
         <code
           dangerouslySetInnerHTML={{
-            __html: hljs.highlight(children, {
-              language: language.toLowerCase() || 'javascript',
-            }).value,
+            __html: htmlText,
           }}
         />
       </pre>
